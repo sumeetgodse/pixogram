3daaa86ef05eef3f269c8a37ebe855b49cafd819
Sumeet Godse <sumeetgodse1997@gmail.com>
2024-11-16 16:58:17 +0530

    fix : use .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDR1vTGLTB4VDbfVyBRD2AbpXon3KQPC-0",
-  authDomain: "pixogram-4186b.firebaseapp.com",
-  projectId: "pixogram-4186b",
-  storageBucket: "pixogram-4186b.appspot.com",
-  messagingSenderId: "166905109330",
-  appId: "1:166905109330:web:5ec542a6495dfffecff88b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 export const storage = getStorage(app)
